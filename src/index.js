88b87bda9305377530619656ec4b202e97bc0fa6
AlinaCheremisina <bochkovaalinyk@yandex.ru>
2022-02-24 20:53:11 +0300

    init: start brackets task. Complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arrFromStr = str.split('');
+
+  const removeChecked = () => {
+    bracketsConfig.forEach((config) => {
+      arrFromStr.forEach((bracket, i) => {
+        if (config[0] === bracket && config[1] === arrFromStr[i + 1]) {
+          arrFromStr.splice(i, 2);
+        };
+      });
+    });
+
+    if (arrFromStr.length) {
+      const results = [];
+
+      bracketsConfig.forEach((config) => {
+        arrFromStr.forEach((bracket, i) => {
+          if (config[0] === bracket && config[1] === arrFromStr[i + 1]) {
+            results.push(true);
+          };
+        });
+      });
+
+      if (results.length) {
+        removeChecked();
+      };
+    };
+  };
+
+  removeChecked();
+
+  if (arrFromStr.length) {
+    return false;
+  } else {
+    return true;
+  };
 }
